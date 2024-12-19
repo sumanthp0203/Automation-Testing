d376fe3f2647abd57cd882bc921af3bba7baa3fd
Sumanth0203 <psumanth0204@gmail.com>
2024-12-18 21:53:51 -0500

    Add Spring Intial files

---
+++ src/main/java/com/automation/Automation/Testing/AutomationTestingApplication.java
@@ -0,0 +1,13 @@
+package com.automation.Automation.Testing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomationTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomationTestingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/automation/Automation/Testing/AutomationTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.automation.Automation.Testing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomationTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
